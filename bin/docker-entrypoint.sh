3131129610d578f0d8b31000cca2ebc90ebfe643
Iulia R Ionita <iulia@ionita.me>
2020-01-20 16:35:41 +0000

    Postcodes to NUTS import should run only if parameter is present

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -5,7 +5,7 @@ bundle exec rails db:migrate
 bundle exec rails db:static
 # temporarily disabling this as it's giving AccessDenied from S3
 #bundle exec rails db:postcode
-bundle exec rails db:run_postcodes_to_nuts_worker
+bundle exec rails db:run_postcodes_to_nuts_worker if ENV['APP_RUN_POSTCODES_TO_NUTS'].present?
 
 bundle exec sidekiq -C ./config/sidekiq.yml -d -L ./log/sidekiq.log -e production if ENV['APP_RUN_SIDEKIQ'].present?
 
